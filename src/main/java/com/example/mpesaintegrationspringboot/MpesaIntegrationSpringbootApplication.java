4e367edda33c6b17f0d969e22f7482f201e4f3d6
murethi <murethibrian@outlook.com>
2023-11-21 23:53:25 +0300

    project boilerplate

---
+++ src/main/java/com/example/mpesaintegrationspringboot/MpesaIntegrationSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.mpesaintegrationspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpesaIntegrationSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpesaIntegrationSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mpesaintegrationspringboot/MpesaIntegrationSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mpesaintegrationspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpesaIntegrationSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
